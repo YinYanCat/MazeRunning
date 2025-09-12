e6aea07026c45157f657c21fd23efd4ddd8777a2
YinYanCat <chyanez2023@udec.cl>
2025-09-12 18:01:57 -0300

    [FEAT]: maze generation using random expansion with greedy walk-back like strategy

---
+++ src/maze/maze.py
@@ -0,0 +1,146 @@
+import random
+import time
+
+import numpy as np
+import pygame
+
+
+
+class Maze:
+    def __init__(self, size=20, cell_size=20, walkback_attempts=0, moves = 400):
+        self.goal_y = None
+        self.goal_x = None
+        self.moves = moves
+        self.walkback_attempts =  walkback_attempts
+        self.size = size
+        self.cell_size = cell_size
+        self.matrix = np.zeros((self.size,self.size))
+
+    def moveObstacles(self):
+        pass
+
+    def next_expandable(self, cursor_x, cursor_y):
+        neighbours = self.cell_neighbours(cursor_x,cursor_y)
+        candidates = [(x,y) for x,y in neighbours if self.matrix[x][y]>0]
+        if not candidates:
+            return None, None
+        min_visits = min(self.matrix[x][y] for x, y in candidates)
+        best = [(x,y) for x,y in candidates if self.matrix[x][y] == min_visits]
+
+        return random.choice(best)
+
+
+    def cell_neighbours(self, cursor_x, cursor_y):
+        neighbours = []
+        if cursor_x < self.size - 1:
+            neighbours.append((cursor_x + 1, cursor_y))
+        if cursor_x > 0:
+            neighbours.append((cursor_x - 1, cursor_y))
+        if cursor_y < self.size -1:
+            neighbours.append((cursor_x, cursor_y + 1))
+        if cursor_y > 0:
+            neighbours.append((cursor_x, cursor_y - 1))
+
+        return neighbours
+
+    def cell_corners(self, cursor_x, cursor_y):
+        corners = []
+        if cursor_x < self.size - 1 and cursor_y < self.size - 1:
+            corners.append((cursor_x + 1, cursor_y + 1))
+        if cursor_x > 0 and cursor_y > 0:
+            corners.append((cursor_x - 1, cursor_y - 1))
+        if cursor_y < self.size - 1 and cursor_x > 0:
+            corners.append((cursor_x - 1, cursor_y + 1))
+        if cursor_y > 0 and cursor_x < self.size - 1:
+            corners.append((cursor_x + 1, cursor_y - 1))
+
+        return corners
+
+    def expand(self, cursor_x, cursor_y):
+        possible_moves = self.cell_neighbours(cursor_x, cursor_y)
+        possible_moves = [(x, y) for x, y in possible_moves if self.matrix[x][y] == 0 and self.is_carveable(cursor_x, cursor_y, x, y)]
+        if possible_moves:
+            cursor_x, cursor_y = random.choice(possible_moves)
+            self.matrix[cursor_x][cursor_y] += 1
+        return cursor_x, cursor_y
+
+    def create_maze(self):
+        self.goal_x,self.goal_y = np.random.randint(0, 20,size=2)
+        self.matrix[self.goal_x][self.goal_y] = -1
+        cursor_x, cursor_y = self.goal_x, self.goal_y
+        moves = self.moves
+
+        while moves > 0:
+            attempts = 0
+            while self.expand(cursor_x, cursor_y) == (cursor_x, cursor_y): # and check not stuck:
+                next_cell = self.next_expandable(cursor_x, cursor_y)
+                if next_cell == (None,None):
+                    continue
+                self.matrix[next_cell] += 1
+                cursor_x,cursor_y = next_cell
+                attempts += 1
+                if attempts >= self.walkback_attempts:
+                    break
+            moves -= 1
+        self.matrix[cursor_x][cursor_y] = -2
+
+    def is_carveable(self, cursor_x, cursor_y, carve_x, carve_y):
+        neigbours = self.cell_neighbours(carve_x,carve_y) + self.cell_corners(carve_x, carve_y)
+
+        if (cursor_x,cursor_y) not in neigbours:
+            return False
+        neigbours.remove((cursor_x, cursor_y))
+        dx, dy = carve_x - cursor_x, carve_y - cursor_y
+        if (dx, dy) == (0, 1) or (dx, dy) == (0, -1):
+            neigbours = [(x,y) for x,y in neigbours if (x,y) not in [(cursor_x - 1, cursor_y),(cursor_x + 1, cursor_y)]]
+        if (dx, dy) == (1, 0) or (dx, dy) == (-1, 0):
+            neigbours = [(x, y) for x, y in neigbours if (x, y) not in [(cursor_x, cursor_y - 1), (cursor_x, cursor_y + 1)]]
+
+
+        for x,y in neigbours:
+            if self.matrix[x][y] > 0:
+                return False
+        return True
+
+    def print_maze(self):
+        for row in self.matrix:
+            print("".join(" " if x > 0 else "■"  for x in row))
+
+    def draw(self, screen):
+        for x in range(self.size):
+            for y in range(self.size):
+                rect = pygame.Rect(y*self.cell_size, x*self.cell_size, self.cell_size, self.cell_size)
+                if self.matrix[x][y] == 0:
+                    color = (0,0,0)  # wall
+                elif self.matrix[x][y] == -1:
+                    color = (255,0,0) # goal
+                elif self.matrix[x][y] == -2:
+                    color = (0,0,255) # starts
+                else:
+                    color = (255/self.matrix[x][y],255/self.matrix[x][y],255/self.matrix[x][y])  # path
+                pygame.draw.rect(screen, color, rect)
+                pygame.draw.rect(screen, (50,50,50), rect, 1)  # borde
+
+
+#TEMPORAL
+
+maze_size = 100
+cell_size = 5
+
+maze = Maze(size=maze_size, cell_size=cell_size, walkback_attempts=100,moves= 5000)
+maze.create_maze()
+
+screen = pygame.display.set_mode((maze_size*cell_size, maze_size*cell_size))
+pygame.display.set_caption("Maze Generation")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((0,0,0))
+    maze.draw(screen)
+    pygame.display.flip()
+    pygame.time.delay(100)
+
+pygame.quit()
